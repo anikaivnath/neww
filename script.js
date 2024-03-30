1615601586a6443621326e111d87eb6f8b8e34f2
anikaivnath <anikaivnath8@gmail.com>
2024-03-31 03:18:53 +0600

    array map

---
+++ script.js
@@ -0,0 +1,23 @@
+let btn= document.getElementById('btn')
+let input=document.querySelector('input')
+let arr=[];
+
+
+btn.addEventListener('click',()=>
+{
+arr.push(input.value);
+arr.map(items=>{
+let p= document.createElement('p');
+p.innerText=items;
+document.body.appendChild(p);
+
+})
+console.log(arr);
+
+input.value='';
+}
+
+
+
+)
+
