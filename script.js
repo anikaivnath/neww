ef46c33669b1338cc3a538c5465cb130042ca5cf
anikaivnath <anikaivnath8@gmail.com>
2024-03-31 03:41:49 +0600

    completed

--- script.js
+++ script.js
@@ -10,12 +10,21 @@ arr.push(input.value);
 arr.map(items=>{
 c++;
 let p= document.createElement('p');
-p.innerText=`${c} ${items} [${new Date().toLocaleTimeString()}]`;
+p.innerText=`${c} ${items} (${new Date().toLocaleTimeString()})`;
 document.body.appendChild(p);
 
-
-
+p.addEventListener('dblclick',()=>
+{
+    p.innerText=`✅ Completed at  (${new Date().toLocaleTimeString()})`;
 })
+
+
+
+
+}
+
+
+)
 
 input.value='';
 arr.pop();
