111cef88d0574d414339452ef0740e3ecf368e90
anikaivnath <anikaivnath8@gmail.com>
2024-03-31 03:35:20 +0600

    add time

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ arr.push(input.value);
 arr.map(items=>{
 c++;
 let p= document.createElement('p');
-p.innerText=`${c} ${items}`;
+p.innerText=`${c} ${items} [${new Date().toLocaleTimeString()}]`;
 document.body.appendChild(p);
 
 
